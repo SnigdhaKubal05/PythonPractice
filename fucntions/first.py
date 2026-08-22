294e42e54d24d71024f11189ba9df8ae2da459ac
SnigdhaKubal05 <snigdhakubal31@gmail.com>
2026-08-22 18:29:15 +0530

    functions 2

--- fucntions/first.py
+++ fucntions/first.py
@@ -1,18 +1,18 @@
 #functions
-#positional arguements
+#1. positional arguements
 def sum(a,b):  #parameters
     print(f"sum of your number is {a+b}")
 
 sum(2,2) #arguements
 sum(5,5)
 
-#default arguements
+#2. default arguements
 def sum(x,y=5):  #parameters
     print(f"sum of your number is {x+y}")
 
 sum(2) 
 
-#keyword arguements
+#3. keyword arguements
 def key(name,age):
     print(f"your name is {name} and age is {age}")
 
