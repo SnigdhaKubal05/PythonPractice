bb977ebdae82d706b96878881f1662ceec1fdb34
SnigdhaKubal05 <snigdhakubal31@gmail.com>
2026-08-24 20:10:09 +0530

    add

---
+++ conditional.py
@@ -0,0 +1,16 @@
+'''p1="make money"
+p2="offer"
+p3="buy now"
+
+message=input("Enter you message: ")
+if((p1 in message) or (p2 in message) or (p3 in message)):
+    print("SPAM HAI BHAGOOOOOOOO")
+else:
+    print("Not spam")
+'''
+
+post=input("Enter you post: ")
+if("snigdha" in post):
+    print("Yes they talking about snigdha")
+else:
+    print("no yaar")
