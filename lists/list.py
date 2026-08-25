f4c90f36a36f53e3748de105323b4517ea2ef927
SnigdhaKubal05 <snigdhakubal31@gmail.com>
2026-08-25 18:37:56 +0530

    lists in python

---
+++ lists/list.py
@@ -0,0 +1,18 @@
+#lists are immutable and hetrogeneous in nature.
+#can store any datatype 
+a=[1,2,3,4,5,6]
+
+print(a)
+print(a[-1])
+
+#traversing
+#1st way - indexing, it prints INDEXES of lists values
+for i in range(len(a)):
+    print(i)
+    #print values
+    print(a[i])
+
+#2nd way - directly values
+for i in a:
+    print(i)
+
---
+++ lists/listMethods.py
@@ -0,0 +1,22 @@
+a=[1,2,3]
+a.append(4)
+
+print(a)
+
+a.insert(2,6)
+
+print(a)
+
+a.remove(2)
+
+print(a)
+
+a.extend([6,7,8,9])
+print(a)
+
+a.pop(3) #removing/poping with index value
+print(a)
+
+a[1]=10
+print(a) #immutable -  not possible in strings
+
