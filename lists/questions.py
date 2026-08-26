d5177ba1276dea67fc2cbe4ef840ce3127df98a4
SnigdhaKubal05 <snigdhakubal31@gmail.com>
2026-08-26 19:14:49 +0530

    lists patterns questions completed

--- lists/questions.py
+++ lists/questions.py
@@ -21,7 +21,7 @@ for i in a:
     sum=sum+i
 
 print(sum/len(a))
-"""
+
 #greatest element with its index
 
 l=[24,29,5,78,100,65]
@@ -34,3 +34,34 @@ for i in range(len(l)):
         largest=l[i]
         index=i
 print(f"Your largest element is {largest} and its index is {index}")
+
+
+#largest and second largest
+a=[12,16,13,19,17]
+
+largest=a[0]
+second_largest=a[0]
+
+for i in a:
+    if i>largest:
+        second_largest=largest
+        largest=i
+
+    elif i>second_largest:
+        second_largest=i
+
+print(second_largest,largest)
+"""
+
+#check if list is sorted or not
+
+b=[4,5,6,7,8]
+
+for i in range(len(b)-1): #because if iteration goes till last value of index i.e 8 in the above list then it will give error of Out of range list
+    if b[i]<b[i+1]:
+        continue
+    else:
+        print("list aint sorted")
+        break
+else:
+    print("list is sorted")
