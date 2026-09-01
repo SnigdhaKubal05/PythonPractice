0e7bdc45b4779a2bb3c2c29f3f4b8bc3d06d787b
SnigdhaKubal05 <snigdhakubal31@gmail.com>
2026-09-01 20:01:33 +0530

    exception handling - raise keyword

--- ExceptionHandling/exceptions.py
+++ ExceptionHandling/exceptions.py
@@ -16,3 +16,15 @@ finally:
 
 print("Will run anyway")
 
+#Raise - manually throwing exception
+
+age=int(input("Enter you age: "))
+try:
+    if age<10 or age>18:
+        raise ValueError("your age must be between 10 and 18")
+    else:
+        print("Welcome to the club")
+except Exception as err:
+    print(f"An error occured as {err}")
+
+print("club opening soon")
