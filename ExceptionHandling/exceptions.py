e46cce2e7fd5ba376bf54e2f4eec9dbe0a0956b6
SnigdhaKubal05 <snigdhakubal31@gmail.com>
2026-09-01 19:54:29 +0530

    exception handling try catch block

--- ExceptionHandling/exceptions.py
+++ ExceptionHandling/exceptions.py
@@ -1,5 +1,18 @@
 a=int(input("enter your number: "))
+try:
+    print(10//a)
 
-print(10//a)
+except Exception as err:        #its like IF, if this does not runs ELSE block will run
+    print(f"sorry lol, enter valid number and not {err}")
 
-print("hi")
+#OR
+
+else:                           #runs only if there us not exception
+    print("There is no exception")
+
+finally:
+    print("I will run no matter what")
+
+
+print("Will run anyway")
+
