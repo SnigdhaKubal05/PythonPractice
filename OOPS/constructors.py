e239233862ab4c3c26daabfaaf8eb16fb1e25585
SnigdhaKubal05 <snigdhakubal31@gmail.com>
2026-09-03 22:49:13 +0530

    constructor in python notes

---
+++ OOPS/constructors.py
@@ -0,0 +1,22 @@
+"""
+object banane ke liye hume class ki zarurat hoti hai;
+jab hum functions banate hai and we need something or requirements
+from users weask it through FUNCTION PARAMETERS
+
+but in CLASS:
+    we want to ask for requirements from users 
+    in class we cannot directly use parameters like:
+        class Factory(PARAMETERS):
+            attributes
+            methods 
+
+        *ABOVE IS THE WRONG*
+    Then,   
+    How can we?
+    ->USING CONSTRUCTOR
+    we can ask for parameters or requirements from the users 
+    it runs automatically when we call a class - constructor function targets the objects' location
+
+    issi ko hum dundar methods bhi kehte hai --> special methods in python that starts and ends with two underscores
+    they are built in
+"""
