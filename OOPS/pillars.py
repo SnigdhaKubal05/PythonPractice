555cd73f865ba0608deb14d0c5eaa42708b43cda
SnigdhaKubal05 <snigdhakubal31@gmail.com>
2026-09-07 20:53:32 +0530

    4 Pillars - constructor inheritence

--- OOPS/pillars.py
+++ OOPS/pillars.py
@@ -21,3 +21,23 @@ print(object2.attribute1)
 object2.method1()
 
 #all the attributes and methods can be access using object of child class as well.
+
+#Contructor Inheritence
+
+class Animal:
+    def __init__(self, name):
+        self.name=name
+
+    def show(self):
+        print(f"name is {self.name}")
+
+class Human(Animal):
+    pass
+
+HumanObj=Human("Snigdha")
+
+AnimalObj=Animal("Lion")
+
+HumanObj.show()
+
+AnimalObj.show()
