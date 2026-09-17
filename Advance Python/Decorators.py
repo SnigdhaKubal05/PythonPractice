c3ed46dc6e7979fc780d70fef6ebf63520c6b44c
SnigdhaKubal05 <snigdhakubal31@gmail.com>
2026-09-17 18:36:20 +0530

    list comprehension

--- Advance Python/Decorators.py
+++ Advance Python/Decorators.py
@@ -19,3 +19,20 @@ def Hello(a,b):
     print(f"Addition is {a+b}")
 
 Hello(5,5)
+
+
+#decorators with args and kwargs - not touching decorator function, but changing the function which is being decorated
+
+def decorate1(func): 
+    def wrapper(*args, **kwargs):
+        print("Before function execution")
+        func(*args,**kwargs)
+        print("After function execution")
+    return wrapper
+
+
+@decorate1
+def Hey(a,b,c):
+    print(f"Addition is {a+b}")
+
+Hey(5,5,5)
---
+++ Advance Python/listComprehension.py
@@ -0,0 +1,12 @@
+l=[i for i in range(0,21) if i%2==0]
+#do this - for this situation - in this condition
+
+print(l)
+
+#ternary operation
+
+x,y=5,5
+
+demo=x if x>y else y
+
+print(demo)
